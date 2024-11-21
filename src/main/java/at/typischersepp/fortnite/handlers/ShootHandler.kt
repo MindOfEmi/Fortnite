a8bda9b88797f014203d24702ff9c38d11a19b9d
Emilian <emilianecker@gmail.com>
2024-11-21 16:08:49 +0100

    feat: Add PacketManager (not working)

--- src/main/java/at/typischersepp/fortnite/handlers/ShootHandler.kt
+++ src/main/java/at/typischersepp/fortnite/handlers/ShootHandler.kt
@@ -1,9 +1,10 @@
 package at.typischersepp.fortnite.handlers
 
 import at.typischersepp.fortnite.Fortnite
-import at.typischersepp.fortnite.utils.getMetadata
-import at.typischersepp.fortnite.utils.reload
-import at.typischersepp.fortnite.utils.setMetadata
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.ProtocolManager
+import com.comphenix.protocol.events.PacketContainer
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.EntityType
@@ -12,39 +13,46 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.persistence.PersistentDataType
+import java.util.*
 
-class ShootHandler(private var plugin: Fortnite) : Listener {
-    // Initialize Handler
+class ShootHandler(var plugin: Fortnite) : Listener {
     init {
         Bukkit.getPluginManager().registerEvents(this, plugin)
     }
 
     @EventHandler
     fun onLeftClick(event: PlayerInteractEvent) {
-        // Check if shoot with gun
         if (!event.action.isLeftClick) return
 
-        // Avoid breaking blocks
         event.isCancelled = true
 
-        // Initialize variable for better understanding
         val player = event.player
         val inventory = player.inventory
 
-        // Check if gun has ammo
-        // If it doesn't have ammo, reload
-        val ammo = getMetadata(inventory.itemInMainHand, NamespacedKey(plugin, "ammo"), PersistentDataType.INTEGER)
-        if (ammo == 0) return reload(player, plugin)
+        val manager: ProtocolManager = ProtocolLibrary.getProtocolManager()
 
-        // Remove shoot from ammo
-        setMetadata(inventory.itemInMainHand, NamespacedKey(plugin, "ammo"), PersistentDataType.INTEGER, ammo - 1)
+        val packet: PacketContainer = manager.createPacket(PacketType.Play.Server.SET_COOLDOWN)
+        packet.integers.write(0, 1)
+        packet.integers.write(1, 40)
+
+        manager.sendServerPacket(player, packet)
+
+        val gun = inventory.itemInMainHand
+        val key = NamespacedKey(plugin, "ammo")
+        val itemMeta = gun.itemMeta
+        val ammo = itemMeta.persistentDataContainer.get(key, PersistentDataType.INTEGER)
+        if (ammo == null || ammo == 0) return
+
+        itemMeta.persistentDataContainer.set(key, PersistentDataType.INTEGER, ammo - 1)
+        gun.setItemMeta(itemMeta)
+
         player.level = ammo - 1
 
-        // Spawn Bullet
         val projectile = player.world.spawnEntity(player.eyeLocation, EntityType.ARROW)
 
-        // Set amount of damage to bullet and launch
-        projectile.setMetadata("damage", FixedMetadataValue(plugin, getMetadata(inventory.itemInMainHand, NamespacedKey(plugin, "damage"), PersistentDataType.INTEGER)))
+        val random = Random()
+
+        projectile.setMetadata("damage", FixedMetadataValue(plugin, random.nextInt(100)))
         projectile.velocity = player.location.direction.multiply(1000)
     }
 }
